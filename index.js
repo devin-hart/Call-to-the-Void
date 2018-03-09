39fc46e0784b332ec33a99edd9b901c52ac18768
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2018-03-09 15:07:59 -0500

    triangle osc running/volume running

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const sineSlider = document.querySelector('.sine-slider');
 const sineVol = document.querySelector('.sine-vol-slider');
 
 const triangleSlider = document.querySelector('.triangle-slider');
-const triangleVol = document.querySelector('.sine-vol-slider');
+const triangleVol = document.querySelector('.triangle-vol-slider');
 
 const sawtoothSlider = document.querySelector('.sawtooth-slider');
-const sawtoothVol = document.querySelector('.sine-vol-slider');
+const sawtoothVol = document.querySelector('.sawtooth-vol-slider');
 
 const pulseSlider = document.querySelector('.pulse-slider');
 const pulseVol = document.querySelector('.pulse-vol-slider');
@@ -17,8 +17,14 @@ const sineOsc = new Tone.Oscillator({ type : "sine",
                                       volume: sineVol.value,
                                       detune : 0 ,
                                       phase : 0 ,
-                                      partials : []
-}).toMaster().start();
+                                      partials : []}).toMaster().start();
+
+const triangleOsc = new Tone.Oscillator({ type : "triangle",
+                                          frequency : triangleSlider.value ,
+                                          volume: triangleVol.value,
+                                          detune : 0 ,
+                                          phase : 0 ,
+                                          partials : []}).toMaster().start();
 
 
 const pulseOsc = new Tone.PWMOscillator({ frequency : pulseSlider.value,
@@ -31,6 +37,10 @@ const pulseOsc = new Tone.PWMOscillator({ frequency : pulseSlider.value,
 sineSlider.addEventListener('input', () => { console.log("Sine frequency " + sineOsc.frequency.value); });
 sineSlider.addEventListener('input', () => { sineOsc.frequency.value = sineSlider.value; });
 sineVol.addEventListener('input', () => { sineOsc.volume.value = sineVol.value });
+
+triangleSlider.addEventListener('input', () => { console.log("Triangle frequency " + triangleOsc.frequency.value); });
+triangleSlider.addEventListener('input', () => { triangleOsc.frequency.value = triangleSlider.value; });
+triangleVol.addEventListener('input', () => { triangleOsc.volume.value = triangleVol.value });
 
 pulseSlider.addEventListener('input', () => { console.log("Pulse frequency " + pulseOsc.frequency.value); });
 pulseSlider.addEventListener('input', () => { pulseOsc.frequency.value = pulseSlider.value; });
