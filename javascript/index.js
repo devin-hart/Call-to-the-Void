6a8c37c0e4d29624a6984c5c6a0767488da4b3c3
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2018-03-10 13:15:53 -0500

    code cleanup

--- javascript/index.js
+++ javascript/index.js
@@ -46,62 +46,61 @@ const pulseOsc = new Tone.PWMOscillator({ frequency : 0,
 // Frequency Knobs
 sineKnob.addEventListener('input', () => {
   sineOsc.frequency.value = sineKnob.value;
-  console.log("Sine frequency: " + sineOsc.frequency.value); });
+  console.log("Sine frequency: " + sineOsc.frequency.value);
+});
 
 triangleKnob.addEventListener('input', () => {
   triangleOsc.frequency.value = triangleKnob.value;
-  console.log("Triangle frequency: " + triangleOsc.frequency.value); });
+  console.log("Triangle frequency: " + triangleOsc.frequency.value);
+});
 
 sawtoothKnob.addEventListener('input', () => {
   sawtoothOsc.frequency.value = sawtoothKnob.value;
-  console.log("Sawtooth frequency: " + sawtoothOsc.frequency.value); });
+  console.log("Sawtooth frequency: " + sawtoothOsc.frequency.value);
+});
 
 pulseKnob.addEventListener('input', () => {
   pulseOsc.frequency.value = pulseKnob.value;
-  console.log("Pulse frequency: " + pulseOsc.frequency.value); });
+  console.log("Pulse frequency: " + pulseOsc.frequency.value);
+});
 
-  // Volume Knobs
-  sineVolKnob.addEventListener('input', () => {
-    sineOsc.volume.value = sineVolKnob.value;
-    console.log("Sine volume: " + Math.ceil(sineOsc.volume.value));
-    lightOn(sineVolKnob.value, sineLight);
-  });
+// Volume Knobs
+sineVolKnob.addEventListener('input', () => {
+  sineOsc.volume.value = sineVolKnob.value;
+  console.log("Sine volume: " + Math.ceil(sineOsc.volume.value));
+  lightOn(sineVolKnob.value, sineLight);
+});
 
-  triangleVolKnob.addEventListener('input', () => {
-    triangleOsc.volume.value = triangleVolKnob.value;
-    console.log("Triangle volume: " + triangleOsc.volume.value);
-    lightOn(triangleVolKnob.value, triangleLight);
-  });
+triangleVolKnob.addEventListener('input', () => {
+  triangleOsc.volume.value = triangleVolKnob.value;
+  console.log("Triangle volume: " + triangleOsc.volume.value);
+  lightOn(triangleVolKnob.value, triangleLight);
+});
 
-  sawtoothVolKnob.addEventListener('input', () => {
-    sawtoothOsc.volume.value = sawtoothVolKnob.value;
-    console.log("Sawtooth volume: " + sawtoothOsc.volume.value);
-    lightOn(sawtoothVolKnob.value, sawtoothLight);
-  });
+sawtoothVolKnob.addEventListener('input', () => {
+  sawtoothOsc.volume.value = sawtoothVolKnob.value;
+  console.log("Sawtooth volume: " + sawtoothOsc.volume.value);
+  lightOn(sawtoothVolKnob.value, sawtoothLight);
+});
 
-  pulseVolKnob.addEventListener('input', () => {
-    pulseOsc.volume.value = pulseVolKnob.value;
-    console.log("Pulse volume: " + pulseOsc.volume.value);
-    lightOn(pulseVolKnob.value, pulseLight);
-  });
-
-  // PWM
-  pwmKnob.addEventListener('input', () => {
-    pulseOsc.modulationFrequency.value = pwmKnob.value;
-    console.log("PWM Duty Cycle: " + pulseOsc.modulationFrequency.value); });
-
-  function lightOn(knobVal, knobElement) {
-    console.log(knobVal);
-    if (Math.ceil(knobVal) > -64) {
-      knobElement.style.backgroundColor = "red";
-      return console.log('this should work');
-    } else {
-      knobElement.style.backgroundColor = "black";
-      console.log('not working')
-    }
-  };
+pulseVolKnob.addEventListener('input', () => {
+  pulseOsc.volume.value = pulseVolKnob.value;
+  console.log("Pulse volume: " + pulseOsc.volume.value);
+  lightOn(pulseVolKnob.value, pulseLight);
+});
 
 // PWM
 pwmKnob.addEventListener('input', () => {
   pulseOsc.modulationFrequency.value = pwmKnob.value;
   console.log("PWM Duty Cycle: " + pulseOsc.modulationFrequency.value); });
+
+function lightOn(knobVal, knobElement) {
+  console.log(knobVal);
+  if (Math.ceil(knobVal) > -64) {
+    knobElement.style.backgroundColor = "red";
+    return console.log('this should work');
+  } else {
+    knobElement.style.backgroundColor = "black";
+    console.log('not working')
+  }
+};
