43af9d0aa642c5f749c69090f9fd161d0e524f1a
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2018-03-09 19:05:23 -0500

    functionality done for now

--- javascript/index.js
+++ javascript/index.js
@@ -1,48 +1,45 @@
 // ***Query Selectors***
+// Sine
 const sineKnob = document.querySelector('#sine-knob');
 const sineVolKnob = document.querySelector('#sine-vol-knob');
 
+// Triangle
 const triangleKnob = document.querySelector('#triangle-knob');
 const triangleVolKnob = document.querySelector('#triangle-vol-knob');
 
+// Sawtooth
 const sawtoothKnob = document.querySelector('#sawtooth-knob');
 const sawtoothVolKnob = document.querySelector('#sawtooth-vol-knob');
 
+// Pulse
 const pulseKnob = document.querySelector('#pulse-knob');
 const pulseVolKnob = document.querySelector('#pulse-vol-knob');
-const pwmKnob = document.querySelector('#pwm-knob')
+const pwmKnob = document.querySelector('#pwm-knob');
 
-// ***Oscillators***
+
+// Oscillators
 const sineOsc = new Tone.Oscillator({ type : "sine",
                                       frequency : 0,
-                                      volume: -64,
-                                      detune : 0,
-                                      phase : 0,
-                                      partials : []}).toMaster().start();
+                                      volume: -64 }).toMaster().start();
 
 const triangleOsc = new Tone.Oscillator({ type : "triangle",
                                           frequency : 0,
-                                          volume: 0,
-                                          detune : 0,
-                                          phase : 0,
-                                          partials : []}).toMaster().start();
+                                          volume: -64 }).toMaster().start();
 
 const sawtoothOsc = new Tone.Oscillator({ type : "sawtooth",
                                           frequency : 0,
-                                          volume: -64,
-                                          detune : 0,
-                                          phase : 0,
-                                          partials : []}).toMaster().start();
+                                          volume: -64 }).toMaster().start();
 
 
 const pulseOsc = new Tone.PWMOscillator({ frequency : 0,
                                           volume: -64,
-                                          detune : 0,
-                                          phase  : 0,
                                           modulationFrequency : 0}).toMaster().start();
 
+
+// LFO
+
 // ***Event Listeners***
-  // Frequency Knobs
+// Frequency Knobs
 sineKnob.addEventListener('input', () => {
   sineOsc.frequency.value = sineKnob.value;
   console.log("Sine frequency: " + sineOsc.frequency.value); });
@@ -59,7 +56,7 @@ pulseKnob.addEventListener('input', () => {
   pulseOsc.frequency.value = pulseKnob.value;
   console.log("Pulse frequency: " + pulseOsc.frequency.value); });
 
-  // Volume Knobs
+// Volume Knobs
 sineVolKnob.addEventListener('input', () => {
   sineOsc.volume.value = sineVolKnob.value;
   console.log("Sine volume: " + sineOsc.volume.value); });
@@ -76,7 +73,7 @@ pulseVolKnob.addEventListener('input', () => {
   pulseOsc.volume.value = pulseVolKnob.value;
   console.log("Pulse volume: " + pulseOsc.volume.value); });
 
-  // PWM
+// PWM
 pwmKnob.addEventListener('input', () => {
   pulseOsc.modulationFrequency.value = pwmKnob.value;
   console.log("PWM Duty Cycle: " + pulseOsc.modulationFrequency.value); });
